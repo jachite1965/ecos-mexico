e7ad32ec1301b06b13f7006301d2629908b69150
jachite1965 <152214503+jachite1965@users.noreply.github.com>
2026-01-27 16:16:14 -0600

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,7 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
-*/
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './index.css';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
